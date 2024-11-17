585b3d5d0837f39105527102a726f19f6bbec10a
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-17 01:52:00 -0500

    setup for health rate

---
+++ ExpertZzz Watch App/OurActionManager.swift
@@ -0,0 +1,13 @@
+//
+//  Manager.swift
+//  ExpertZzz Watch App
+//
+//  Created by Mami Ravaloarison on 11/16/24.
+//
+
+import Foundation
+import HealthKit
+
+class OurActionManager: NSObject, ObservableObject {
+    
+}
