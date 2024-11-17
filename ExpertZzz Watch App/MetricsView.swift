06deaf0730c82fe5c338f60cf9f0eab836083d23
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-16 21:53:27 -0500

    cleanning Metrics Text styles code

--- ExpertZzz Watch App/MetricsView.swift
+++ ExpertZzz Watch App/MetricsView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct MetricsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("03:15.23")
+                .foregroundStyle(Color.yellow)
+                .commonTextStyle(.title2)
+            Text(
+                153.formatted(
+                    .number.precision(.fractionLength(0))
+                )
+                + " bpm"
+            )
+            .commonTextStyle(.title)
+        }
+        .scenePadding()
+    }
+}
+
+extension View {
+    func commonTextStyle(_ style: Font.TextStyle) -> some View {
+        self.font(
+            .system(style, design: .rounded)
+                .monospaced()
+                .lowercaseSmallCaps()
+        )
     }
 }
 
