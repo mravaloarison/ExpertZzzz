a063aaaf08b531598bb1236b18128d5e40247e2d
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-16 23:52:49 -0500

    Summary view added

---
+++ ExpertZzz Watch App/SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  ExpertZzz Watch App
+//
+//  Created by Mami Ravaloarison on 11/16/24.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryView()
+}
