3fcd93f6315b5b714e06fe333c50812876096cee
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-16 21:47:40 -0500

    metrcis added

---
+++ ExpertZzz Watch App/ControlsView.swift
@@ -0,0 +1,18 @@
+//
+//  ControlsView.swift
+//  ExpertZzz Watch App
+//
+//  Created by Mami Ravaloarison on 11/16/24.
+//
+
+import SwiftUI
+
+struct ControlsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlsView()
+}
